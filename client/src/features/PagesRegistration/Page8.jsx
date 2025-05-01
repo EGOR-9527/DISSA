5bf00690d9fb7ad258a05ded124211268c2a33c3
Egor Docenko <egordocenko359@gmail.com>
2025-05-02 01:42:18 +0300

    сделал страницу 8

--- client/src/features/PagesRegistration/Page8.jsx
+++ client/src/features/PagesRegistration/Page8.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import logo from "../../svg/logo.svg";
+import registrationStyle from "../../css/registration.module.css";
 
 const Page8 = () => {
-    return (
-        <div>
-            8
-        </div>
-    );
+  return (
+    <div className={registrationStyle.describeYourselfContainer}>
+      <div className={registrationStyle.logoHeader}>
+        <img src={logo} alt="Логотип" />
+        <p>DISSA</p>
+      </div>
+
+      <div className={registrationStyle.describeYoursel}>
+        <h2>
+          Расскажи
+          <br />
+          что-нибудь о себе
+        </h2>
+        <textarea className={registrationStyle.textarea} maxLength={225} type="text" placeholder="Введите текст..." />
+      </div>
+    </div>
+  );
 };
 
 export default Page8;
