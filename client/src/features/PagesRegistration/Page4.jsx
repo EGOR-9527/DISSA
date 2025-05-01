d0f558900e5fe4c4c0393b098d01a47b95d188e5
Egor Docenko <egordocenko359@gmail.com>
2025-05-01 23:28:58 +0300

    Доделал 4 строницу

--- client/src/features/PagesRegistration/Page4.jsx
+++ client/src/features/PagesRegistration/Page4.jsx
@@ -1,42 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../svg/logo.svg";
 import registrationStyle from "../../css/registration.module.css";
 
-const days = Array.from({ length: 31 }, (_, i) => i + 1);
 const months = Array.from({ length: 12 }, (_, i) => i + 1);
 const years = Array.from(
-  { length: 100 },
+  { length: 60 },
   (_, i) => new Date().getFullYear() - i
 );
 
 const Page4 = () => {
-  const [day, setDay] = useState(null);
+  const [day, setDay] = useState(30);
   const [month, setMonth] = useState(null);
   const [year, setYear] = useState(null);
-  const [daysInMonth, setDaysInMonth] = useState([]);
 
-  // Функция для получения количества дней в месяце
-  const getDaysInMonth = (month, year) => {
-    const daysInMonth = new Date(year, month, 0).getDate();
-    setDaysInMonth(Array.from({ length: daysInMonth }, (_, i) => i + 1));
-  };
+  const [selectedDay, setSelectedDay] = useState(null);
 
-  const handleDaySelect = (day) => {
-    setDay(day);
-  };
+  const [daysInMonth, setDaysInMonth] = useState(
+    Array.from({ length: 30 }, (_, i) => i + 1)
+  );
 
-  const handleMonthSelect = (month) => {
-    setMonth(month);
-    if (year) {
-      getDaysInMonth(month, year);
+  const [openDays, setOpenDays] = useState(false);
+  const [openMonths, setOpenMonths] = useState(false);
+  const [openYears, setOpenYears] = useState(false);
+
+  const [filteredYears, serFilteredYears] = useState([]);
+
+  useEffect(() => {
+    const newYear = new Date().getFullYear();
+    serFilteredYears(years.filter((y) => newYear - y >= 16));
+  }, []);
+
+  useEffect(() => {
+    if (month && year) {
+      const days = new Date(year, month, 0).getDate();
+      const newDays = Array.from({ length: days }, (_, i) => i + 1);
+      setDaysInMonth(newDays);
+      if (day > days) setDay(null);
     }
+  }, [month, year]);
+
+  const handleDaySelect = (d) => {
+    setDay(d);
+    setSelectedDay(d);
+    setOpenDays(false);
   };
 
-  const handleYearSelect = (year) => {
-    setYear(year);
-    if (month) {
-      getDaysInMonth(month, year);
-    }
+  const handleMonthSelect = (m) => {
+    setMonth(m);
+    setOpenMonths(false);
+  };
+
+  const handleYearSelect = (y) => {
+    setYear(y);
+    setOpenYears(false);
   };
 
   return (
@@ -46,13 +62,64 @@ const Page4 = () => {
         <p>DISSA</p>
       </div>
 
-      <div className={registrationStyle.dateInputBlock}>
-        <div className={registrationStyle.dateSelectorWrapper}>
-          <h2 className={registrationStyle.title}>
-            Твоя дата
-            <br />
-            рождения
-          </h2>
+      <div className={registrationStyle.containerDate}>
+        <h2 className={registrationStyle.title}>
+          Твоя дата
+          <br />
+          рождения
+        </h2>
+
+        {/* День */}
+        <div className={registrationStyle.dateInut}>
+          <p onClick={() => setOpenDays((prev) => !prev)}>
+            {selectedDay || "ДД"}
+          </p>
+          <div className={registrationStyle.menu}>
+            {openDays &&
+              daysInMonth.map((d) => (
+                <div
+                  onClick={() => handleDaySelect(d)}
+                  key={d}
+                  className={registrationStyle.day}
+                >
+                  {d}
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Месяц */}
+        <div className={registrationStyle.dateInut}>
+          <p onClick={() => setOpenMonths((prev) => !prev)}>{month || "ММ"}</p>
+          <div className={registrationStyle.menu}>
+            {openMonths &&
+              months.map((m) => (
+                <div
+                  onClick={() => handleMonthSelect(m)}
+                  key={m}
+                  className={registrationStyle.day}
+                >
+                  {m}
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Год */}
+        <div className={registrationStyle.dateInut}>
+          <p onClick={() => setOpenYears((prev) => !prev)}>{year || "ГГГГ"}</p>
+          <div className={registrationStyle.menu}>
+            {openYears &&
+              filteredYears.map((y) => (
+                <div
+                  onClick={() => handleYearSelect(y)}
+                  key={y}
+                  className={registrationStyle.day}
+                >
+                  {y}
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
